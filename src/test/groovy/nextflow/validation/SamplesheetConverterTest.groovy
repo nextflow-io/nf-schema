2d70920b3fcc795f11832a2ff178f79d58ac3caa
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2025-07-02 11:06:16 +0200

    sanitize CSV and TSV files to prevent empty header columns

--- src/test/groovy/nextflow/validation/SamplesheetConverterTest.groovy
+++ src/test/groovy/nextflow/validation/SamplesheetConverterTest.groovy
@@ -641,4 +641,52 @@ class SamplesheetConverterTest extends Dsl2Spec{
         stdout.contains("[[mapMeta:this is in a map, arrayMeta:[metaString45, metaString478], otherArrayMeta:[metaString45, metaString478], meta:metaValue, metaMap:[entry1:entry1String, entry2:12.56]], [[string1, string2], string3, 1, 1, ${getRootString()}/file1.txt], [string4, string5, string6], [[string7, string8], [string9, string10]], test]" as String)
 
     }
+
+    def 'samplesheetToList - correctly sanitize empty header columns CSV' () {
+        given:
+        def SCRIPT_TEXT = '''
+            include { samplesheetToList } from 'plugin/nf-schema'
+
+            workflow {
+                Channel.fromList(samplesheetToList("src/testResources/samplesheet_empty_header_column.csv", "src/testResources/no_meta_schema.json")).view()       
+            }
+
+        '''
+
+        when:
+        dsl_eval(SCRIPT_TEXT)
+        def stdout = capture
+                .toString()
+                .readLines()
+                .findResults {it.startsWith('[') ? it : null }
+
+        then:
+        noExceptionThrown()
+        stdout.contains("[file1.txt, file2.txt]")
+
+    }
+
+    def 'samplesheetToList - correctly sanitize empty header columns TSV' () {
+        given:
+        def SCRIPT_TEXT = '''
+            include { samplesheetToList } from 'plugin/nf-schema'
+
+            workflow {
+                Channel.fromList(samplesheetToList("src/testResources/samplesheet_empty_header_column.tsv", "src/testResources/no_meta_schema.json")).view()       
+            }
+
+        '''
+
+        when:
+        dsl_eval(SCRIPT_TEXT)
+        def stdout = capture
+                .toString()
+                .readLines()
+                .findResults {it.startsWith('[') ? it : null }
+
+        then:
+        noExceptionThrown()
+        stdout.contains("[file1.txt, file2.txt]")
+
+    }
 }
